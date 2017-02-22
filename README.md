c564da704184f72daac09ac84e747a9ad536ef27
Danielle Roberson <danielle.roberson@icloud.com>
2017-02-22 14:40:55 -0800

    SVG Primer
    SVG as Image
    SVG as Code
    Animate with CSS

--- README.md
+++ README.md
@@ -3,8 +3,3 @@
 ## Brainstorming Ideas
 
 * National Corgi Day -- 20th of April
-* I Forgot Day -- July 2	
-* National Black Cat Appreciation Day -- August 17th
-* National Wolf Awareness Week -- OCTOBER 16 - OCTOBER 22
-* Wolf Awareness Day -- October 17
-* National Hug Your Wolf Day -- 8th of June
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 // YOUR SCRIPTS GO HERE
+
+var img0 = '../img/200-1.jpg';
+var img1 = '../img/200-2.jpg';
+var img2 = '../img/200-3.jpg';
+
+// Get Random Number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+// Display randomized images
+$('#random').attr( 'src', eval('img' + numRand));
