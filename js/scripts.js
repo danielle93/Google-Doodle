d4c2581a725262654deaf23e04a7ee65fe104a66
Danielle Roberson <danielle.roberson@icloud.com>
2017-02-23 20:58:16 -0800

    Fixing Image Path Test 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 // YOUR SCRIPTS GO HERE
 
-var img0 = '../img/corg0.svg';
-var img1 = '../img/corg1.svg';
-var img2 = '../img/corg2.svg';
-var img3 = '../img/corg3.svg';
-var img4 = '../img/corg4.svg';
-var img5 = '../img/corg5.svg';
-var img6 = '../img/corg6.svg';
-var img7 = '../img/corg7.svg';
+var img0 = 'img/corg0.svg';
+var img1 = 'img/corg1.svg';
+var img2 = 'img/corg2.svg';
+var img3 = 'img/corg3.svg';
+var img4 = 'img/corg4.svg';
+var img5 = 'img/corg5.svg';
+var img6 = 'img/corg6.svg';
+var img7 = 'img/corg7.svg';
 
 // Get Random Number
 var max = 8;
